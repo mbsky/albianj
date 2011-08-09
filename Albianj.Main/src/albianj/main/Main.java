34a3514406496c7e65c679edd7363141eed7cd73
xvhfeng <xvhfeng@gmail.com>
2011-08-09 15:29:13 +0000

    modify the configure file

--- Albianj.Main/src/albianj/main/Main.java
+++ Albianj.Main/src/albianj/main/Main.java
@@ -8,6 +8,10 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.albianj.io.*;
+import org.albianj.kernel.AlbianBootService;
+import org.albianj.kernel.AlbianServiceRouter;
+import org.albianj.kernel.AlbianState;
+import org.albianj.logger.IAlbianLoggerService;
 import org.albianj.service.AlbianServiceException;
 import org.albianj.xml.XmlParser;
 import org.apache.log4j.Logger;
@@ -28,46 +32,69 @@ public class Main
 	static Logger logger = Logger.getLogger(Main.class.getName()); 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		String path = null;
 		try
 		{
-			DOMConfigurator.configure(Path.getExtendResourcePath("../config/log4j.xml"));
-			logger.info("aasdsadasd");
-			try
+			AlbianBootService.start();
+			while(AlbianState.Running != AlbianBootService.getLifeState())
 			{
-				Document doc = XmlParser.load(Path.getExtendResourcePath("../config/service.xml"));
-				List nodes = XmlParser.analyze(doc, "Services/Service");
-				 Iterator it=nodes.iterator();   
-			     while(it.hasNext()){   
-			       Element ele=(Element)it.next();         
-			       //System.out.println(ele.getText());
-			       String base = XmlParser.getAttributeValue(ele, "Base");
-			       System.out.println(base);
-			       logger.info(base);
-			     }   
-				return;
+				Thread.sleep(1000);
 			}
-			catch (Exception e)			
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			IAlbianLoggerService logger = AlbianServiceRouter.getService(IAlbianLoggerService.class, "logger");
+			logger.info("i","heat","java");
+			return;
 		}
-		catch (IOException e)
+		catch (Exception e)
 		{
-			
 			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		catch (URISyntaxException e)
-		{
-			
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(path);
-		return;
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+//		String path = null;
+//		try
+//		{
+////			DOMConfigurator.configure(Path.getExtendResourcePath("../config/log4j.xml"));
+//			logger.info("aasdsadasd");
+////			try
+////			{
+//////				Document doc = XmlParser.load(Path.getExtendResourcePath("../config/service.xml"));
+//////				List nodes = XmlParser.analyze(doc, "Services/Service");
+//////				 Iterator it=nodes.iterator();   
+//////			     while(it.hasNext()){   
+//////			       Element ele=(Element)it.next();         
+//////			       //System.out.println(ele.getText());
+//////			       String base = XmlParser.getAttributeValue(ele, "Base");
+//////			       System.out.println(base);
+//////			       logger.info(base);
+////			     }   
+//				return;
+//			}
+//			catch (Exception e)			
+//			{
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		catch (IOException e)
+//		{
+//			
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		catch (URISyntaxException e)
+//		{
+//			
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		System.out.println(path);
+//		return;
 
 	}
 
