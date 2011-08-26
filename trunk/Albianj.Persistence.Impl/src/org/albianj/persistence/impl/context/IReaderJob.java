de9ef8a821fbda270e9be755f16830bafc2f5869
xvhfeng@gmail.com <xvhfeng@gmail.com@ad5b83f8-cb5a-0bdf-7934-13f8396944bf>
2011-08-26 15:41:07 +0000

    add the named parameter

---
+++ trunk/Albianj.Persistence.Impl/src/org/albianj/persistence/impl/context/IReaderJob.java
@@ -0,0 +1,19 @@
+package org.albianj.persistence.impl.context;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.albianj.persistence.impl.db.ICommand;
+import org.albianj.persistence.object.IStorageAttribute;
+
+public interface IReaderJob
+{
+	public IStorageAttribute getStorage();
+	public void setStorage(IStorageAttribute storage);
+	public ICommand getCommand();
+	public void setCommand(ICommand command);
+	public Connection getConnection();
+	public void setConnection(Connection connection);
+	public Statement getStatement();
+	public void setStatement(Statement statement);
+}
