7cd4835c8e11bc7d6cbc27f0cd61cf2d3743980f
xvhfeng@gmail.com <xvhfeng@gmail.com@ad5b83f8-cb5a-0bdf-7934-13f8396944bf>
2011-08-28 10:29:00 +0000

    over the kernel id length work

--- trunk/Albianj.Kernel/src/org/albianj/service/AlbianKeyService.java
+++ trunk/Albianj.Kernel/src/org/albianj/service/AlbianKeyService.java
@@ -1,6 +1,11 @@
 package org.albianj.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
+
+import org.albianj.kernel.KernelSetting;
+import org.albianj.text.StringHelper;
 
 public class AlbianKeyService
 {
@@ -11,10 +16,24 @@ public class AlbianKeyService
 	{
 		return generate("Kenerl");
 	}
-	
+	static int serial = 0;
 	public synchronized static String generate(String appName)
 	{
-		return null;
+		serial = 10000 == serial ? 0 : serial+1; 
+		SimpleDateFormat dateFormat=new SimpleDateFormat("yyyyMMddHHmmss"); 
+		String app = appName;
+		if(app.length() < 7)
+		{
+			app = StringHelper.padLeft(app, 7);
+		}
+		if(app.length() > 7)
+		{
+			app = app.substring(0,7);
+		}
+		
+		return String.format("%1$s-%2$s-%3$s-%4$04d",
+				StringHelper.padLeft(KernelSetting.getKernelId(),4),app,dateFormat.format(new Date())
+				,serial);
 	}
 	
 	public synchronized static String generate32UUID()
