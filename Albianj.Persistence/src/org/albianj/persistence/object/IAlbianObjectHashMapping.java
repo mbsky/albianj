90b4acf7add5d69951b1d1f0658e46911a89e66c
xvhfeng <xvhfeng@gmail.com>
2011-08-28 08:16:44 +0000

    modify the hash function

--- Albianj.Persistence/src/org/albianj/persistence/object/IAlbianObjectHashMapping.java
+++ Albianj.Persistence/src/org/albianj/persistence/object/IAlbianObjectHashMapping.java
@@ -1,9 +1,14 @@
 package org.albianj.persistence.object;
 
+import java.util.List;
+
+
 public interface IAlbianObjectHashMapping
 {
 	public String[] mappingWriterRouting(IAlbianObject obj);
-	public String mappingReaderRouting(IAlbianObject obj);
-	public String mappingWriterTable(IAlbianObject obj,String routingName);
-	public String mappingReaderTable(IAlbianObject obj,String routingName);
+	public String mappingReaderRouting(List<IFilterCondition> wheres,
+			List<IOrderByCondition> orderbys);
+	public String mappingWriterTable(String routingName,IAlbianObject obj);
+	public String mappingReaderTable(String routingName,List<IFilterCondition> wheres,
+			List<IOrderByCondition> orderbys);
 }
