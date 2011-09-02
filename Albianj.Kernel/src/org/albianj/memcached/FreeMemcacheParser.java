f3c55fcc7a55a5897f2a15af9c49c02a989803dd
xvhfeng <xvhfeng@gmail.com>
2011-09-02 13:41:34 +0000

    add the memcache client

---
+++ Albianj.Kernel/src/org/albianj/memcached/FreeMemcacheParser.java
@@ -0,0 +1,37 @@
+package org.albianj.memcached;
+
+import java.util.List;
+
+import org.albianj.io.Path;
+import org.albianj.logger.AlbianLoggerService;
+import org.albianj.service.FreeAlbianService;
+import org.albianj.xml.IParser;
+import org.albianj.xml.XmlParser;
+import org.dom4j.Document;
+
+public abstract class FreeMemcacheParser extends FreeAlbianService implements IParser
+{
+	private final static String path = "../config/memcached.xml";
+	private final static String tagName = "Memcached/Group";
+	public void init()
+	{
+		Document doc = XmlParser.load(Path.getExtendResourcePath(path));
+		if(null == doc)
+		{
+			throw new RuntimeException("load memcache config is error.");
+		}
+		@SuppressWarnings("rawtypes")
+		List nodes = XmlParser.analyze(doc, tagName);
+		if (null == nodes || 0 == nodes.size())
+		{
+			String msg = String.format("There is not %1$s nodes.", tagName);
+				AlbianLoggerService.error(msg);
+			throw new UnsupportedOperationException(msg);
+		}
+//		List<IMemcacheGroup> groups = 
+				parserGroups(nodes);
+		return;
+	}
+	
+	protected abstract void parserGroups(List nodes);
+}
